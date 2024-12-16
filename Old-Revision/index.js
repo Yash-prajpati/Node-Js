06a3adb7566d96b00f376b20f83421844ce595fa
Yash-prajapati <yashprajapati290208@gmail.com>
2024-12-16 10:36:13 +0530

    first commit

---
+++ Old-Revision/index.js
@@ -0,0 +1,17 @@
+console.log("nacho");
+
+let a=10;
+console.log(a);
+
+const object={
+    name:"yash",
+    age:20,
+    city:"surat",
+    hobbies: ["cricket","football","reading"],
+    address:{
+        street:"gandhinagar",
+        city:"surat",
+        }
+}
+
+console.log(object);
