9ab51a4dd28c93f4b9988db69bef1754f4a1172b
Yash-prajapati <yashprajapati290208@gmail.com>
2024-12-16 10:45:01 +0530

    12/6/2024

--- Old-Revision/index.js
+++ Old-Revision/index.js
@@ -15,3 +15,30 @@ const object={
 }
 
 console.log(object);
+
+
+let playerName = "Virat Kohli"; 
+
+
+let scores = [34, 75, 102, 48, 87]; 
+
+
+let doubleScores = scores.map(score => score * 2);
+console.log("Double Scores:", doubleScores); 
+
+
+let highScores = scores.filter(score => score >= 50);
+console.log("High Scores:", highScores); 
+
+
+
+console.log("Player Name:", playerName); 
+
+console.log("Original Scores:", scores); 
+
+
+let arry=[10,4,,6,1];
+
+arry.map((val)=>{
+    console.log(val);
+})
