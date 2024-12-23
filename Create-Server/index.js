d8a38d0a7d39fb494113a83590aa9ccb7de9362f
Yash-prajapati <yashprajapati290208@gmail.com>
2024-12-23 11:11:00 +0530

    12/23/2024

--- Create-Server/index.js
+++ Create-Server/index.js
@@ -1,30 +1,42 @@
 const http = require('http');
-const port = 8001;
+const port = 8000;
 const fs = require('fs');
-const { log } = require('console');
 const server = http.createServer((req,res)=>{
 
-    let filname="";
+    let fileName="";
     switch(req.url){
         case "/":
-            filname = '.home.html'
+            fileName = "./home.html";
+        break;
+        case "/about":
+            fileName = "./about.html";
+        break;
+        case "/contact":
+            fileName = "./contact.html";
+        break;
+        case "/blog":
+            fileName = "./blog.html";
+        break;
+        case "/shop":
+            fileName = "./shop.html";
         break;
     }
 
-    fs.readFile(filename,(err,pagename)=>{
+    fs.readFile(fileName,(err,pagename)=>{
         if(err){
             console.log('file is not found');
-            return false
+            return false;
         }
-        res.end(pagename)
+        res.end(pagename);
     })
 
-    server.listen(port,(err)=>{
+})
+
+  server.listen(port,(err)=>{
         if(!err){
 
-            console.log('server is start on port :-');
+            console.log(`thise server is run:-${port}`);
             
         }
     })
 
-})
