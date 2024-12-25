e52aae72236c6e9270e0e45a83c391f901659dbf
Yash-prajapati <yashprajapati290208@gmail.com>
2024-12-25 10:49:56 +0530

    12/25/2024

--- Express/index.js
+++ Express/index.js
@@ -2,13 +2,22 @@ const express = require('express');
 const port=8070;
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    res.end();
-});
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    return res.render('home')
+})
+
+app.get('/about',(req,res)=>{
+    return res.render('about')
+})
+
+app.get('/contact',(req,res)=>{
+    return res.render('contact')
+})
 
 app.listen(port,(err)=>{
-    if(!err){
+    if(err){
         console.log(`server was run :-${port}`);
         return false;
     }
